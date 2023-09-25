a25197e433aec91b87f2f50b1a2dd4469150893a
TobyThorneA <igraman_XX@mail.ru>
2023-09-25 11:39:04 +0300

    Поставил реакт, установил роутер, тулкит подготовил компоненты для списка задач и создания задачи

---
+++ task-list/src/components/taskList/taskList.tsx
@@ -0,0 +1,11 @@
+import './taskList.scss'
+
+const TaskList = () =>  {
+  return (
+    <section className='wrapper'>
+      <div className='wrapper__tasks'>asdsad</div>
+    </section>
+  )
+}
+
+export default TaskList
